c498ee20a8f50accab58d83cad181afda4ef0e04
sessaluca <sessa.luca@iisgalvanimi.edu.it>
2020-01-30 13:04:16 +0000

    6.02

---
+++ RestFull-CRUD/routes/users.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+const sql = require('mssql')
+const config = {
+  user: 'sessa.luca',  //Vostro user name
+  password: 'xxx123#', //Vostra password
+  server: "213.140.22.237",  //Stringa di connessione
+  database: 'sessa.luca', //(Nome del DB)
+}
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  sql.connect(config, err => {
+    if(err) console.log(err);  // ... error check
+    
+    // Query
+    let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
+    sqlRequest.query('select * from dbo.[cr-unit-attributes]', (err, result) => {
+        if (err) console.log(err); // ... error checks
+        res.send(result);  //Invio il risultato
+    });
+  });
+});
+
+module.exports = router;
